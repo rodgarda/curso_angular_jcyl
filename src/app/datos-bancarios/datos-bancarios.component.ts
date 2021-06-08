2f59de2d84c2349434ea7f5e33b70aff9266d545
rodgarda <rodgarda@outlook.com>
2021-06-08 10:40:31 +0200

    Ejercicio24

--- src/app/datos-bancarios/datos-bancarios.component.ts
+++ src/app/datos-bancarios/datos-bancarios.component.ts
@@ -10,16 +10,25 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DatosBancariosComponent implements OnInit {
     f: FormGroup;
+    pais: string;
+    iban: string;
+    entidad: number;
+    sucursal: number;
+    dc: number
+    cuenta: number;
     constructor(private formsBuilder: FormBuilder) {
       this.f = formsBuilder.group({ pais: '', iban: '' , entidad: '' , sucursal: '' , dc: '' , cuenta: '' });
     }
 
   ngOnInit(): void {
+    this.pais='ES'
+    this.iban='10'
   }
   submit(){
     //console.log(f);
     //console.log(f.form.controls.cuenta.value)
     console.log(this.f);
     console.log(this.f.controls.cuenta.value);
+    console.log(this.pais);
   }
 }
